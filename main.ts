69d7f44c7e50f795be7d5438f13be02638367c48
Kelvin Lee <yckelvin@users.noreply.github.com>
2023-03-23 13:31:43 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    MuseIoT.sendMusespeak(
+    "ML1234",
+    input.lightLevel(),
+    0,
+    0,
+    0,
+    0,
+    0
+    )
+})
+basic.showNumber(0)
+MuseIoT.initializeWifi()
+basic.pause(5000)
+basic.showNumber(1)
+MuseIoT.setWifi("wifi-name", "password")
+basic.pause(10000)
+basic.showNumber(2)
+MuseIoT.ConnectMuseDataMQTTbroker("ML1234")
+basic.pause(2000)
+basic.showNumber(3)
